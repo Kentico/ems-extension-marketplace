bcb0901c60e8c59f934781aac27e37c097f01382
martinm@kentico.com <martinm@kentico.com>
2019-07-24 14:21:13 +0200

    Fix tslint errors.

--- tests/unit/MarketplaceItemDetail.spec.ts
+++ tests/unit/MarketplaceItemDetail.spec.ts
@@ -20,7 +20,7 @@ describe("MarketplaceItemDetail.vue", () => {
       propsData: { item }
     });
     expect(wrapper.html()).toEqual(
-      `<div class=\"marketplace-item\"><div class=\"marketplace-item-header\"><h3 class=\"marketplace-item-header__title\"><a>dummyName</a></h3> <span class=\"marketplace-item-header__source\">by dummySource</span></div> <div class=\"marketplace-item-content\"><div class=\"marketplace-item-content__description\"><p>dummyDescription</p></div> <div class=\"marketplace-item-content__preview-image\"><a href=\"https://localhost.local\"><img src=\"https://localhost.local/thumbnailUrl.png\" alt=\"dummyName\" style=\"width: 100px; height: 100px;\"></a></div> <div class=\"marketplace-item-content__statistics\"></div></div> <div class=\"marketplace-item-footer\"><button data-tracking-label=\"dummyName\" href=\"https://localhost.local\" class=\"btn marketplace-item-content__action\">
+      `<div class="marketplace-item"><div class="marketplace-item-header"><h3 class="marketplace-item-header__title"><a>dummyName</a></h3> <span class="marketplace-item-header__source">by dummySource</span></div> <div class="marketplace-item-content"><div class="marketplace-item-content__description"><p>dummyDescription</p></div> <div class="marketplace-item-content__preview-image"><a href="https://localhost.local"><img src="https://localhost.local/thumbnailUrl.png" alt="dummyName" style="width: 100px; height: 100px;"></a></div> <div class="marketplace-item-content__statistics"></div></div> <div class="marketplace-item-footer"><button data-tracking-label="dummyName" href="https://localhost.local" class="btn marketplace-item-content__action">
       Take me to the Project
     </button></div></div>`
     );
